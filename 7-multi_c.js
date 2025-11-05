a00b409db2d60dc70161bc7f5bd92cc31ca96361
Mustapha Nyass <Mustaphanyass928@gmail.com>
2025-11-05 10:59:45 +0000

    7-multi_c: Fix behavior for negative number

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,9 +1,9 @@
 // 7-multi_c.js
 
-const args = process.argv.slice(2); // get command-line arguments
-const x = parseInt(args[0], 10);    // convert first argument to integer
+const args = process.argv.slice(2);  // get command-line arguments
+const x = parseInt(args[0], 10);     // convert first argument to integer
 
-if (isNaN(x) || x < 1) {
+if (isNaN(x)) {
     console.log("Missing number of occurrences");
 } else {
     for (let i = 0; i < x; i++) {
