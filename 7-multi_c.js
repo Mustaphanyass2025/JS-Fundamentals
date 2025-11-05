39cf4100156e064c9373191e8368642bdae5d259
Mustapha Nyass <Mustaphanyass928@gmail.com>
2025-11-05 10:51:10 +0000

    Add 7-multi_c.js

---
+++ 7-multi_c.js
@@ -0,0 +1,12 @@
+// 7-multi_c.js
+
+const args = process.argv.slice(2); // get command-line arguments
+const x = parseInt(args[0], 10);    // convert first argument to integer
+
+if (isNaN(x) || x < 1) {
+    console.log("Missing number of occurrences");
+} else {
+    for (let i = 0; i < x; i++) {
+        console.log("C is fun");
+    }
+}
